f2ad43b2183073fe5696282f7417537a15e27962
Lawrence <lawrencerodriques06@gmail.com>
2022-12-15 19:35:29 +0530

    Third

---
+++ client/src/Pages/Auth/Auth.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import './Auth.css'
+import icon from '../../assets/icon.png'
+
+const Auth = () => {
+
+    const [isSignup, setIsSignup] = useState(false)
+  return (
+    <section class='auth-section'>
+        <div class='auth-container'>
+        
+            { !isSignup && <img src={icon} alt='stack overflow' className='login-logo'/>}
+            <form>
+                <label htmlFor="email">
+                    <h4>Email</h4>
+                    <input type="email" name='email' id='email'/>
+                </label>
+                <label htmlFor="password">
+                    <h4>Email</h4>
+                    <input type="password" name='password' id='password'/>
+                </label>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default Auth
